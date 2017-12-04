f3a3a97798389e804c696dcb54fde9f7f6df2e2a
Reid <rmcc0724@gmail.com>
2017-12-04 20:19:31 +0000

    yes

--- js/home.js
+++ js/home.js
@@ -3,12 +3,7 @@
 
 var html1 = "<ul id=gallery>";
 
-html1 +=    "<li>";
-html1 +=    "<a href=http://news.keuriggreenmountain.com/>";
-html1 +=    "<img src=images/keurig.png alt=Keurig.org>";
-html1 +=    "<p>Keurig News HQ</p>";
-html1 +=    "</a>";
-html1 +=    "</li>";
+
 
 html1 +=    "<li>";
 html1 +=    "<a href=http://www.girlsingerjoyce.com>";
@@ -24,12 +19,6 @@ html1 +=    "<p>Make the best coffee</p>";
 html1 +=    "</a>";
 html1 +=    "</li>";
 
-html1 +=    "<li>";
-html1 +=    "<a href=http://newsroom.mgm.mo/>";
-html1 +=    "<img id=mgm src=images/mgm2.png alt=MGM Macau>";
-html1 +=    "<p>MGM Macau News HQ</p>";
-html1 +=    "</a>";
-html1 +=    "</li>";
 
 html1 +=    "<li>";
 html1 +=    "<a href=../../craps/new_craps.html>";
