fc0ea1b6f9a1866172f9af7a5ba8c2f7688200fb
Reid <rmcc0724@gmail.com>
2018-06-27 19:26:15 +0000

    Update to photos

--- js/home.js
+++ js/home.js
@@ -4,33 +4,68 @@
 var html1 = "<ul id=gallery>";
 
 
+html1 +=    "<li>";
+html1 +=    "<a href=http://newsroom.mgm.mo>";
+html1 +=    "<img src=images/mgm2.png alt=MGM Macau>";
+html1 +=    "<p>MGM Macau</p>";
+html1 +=    "</a>";
+html1 +=    "</li>";
+
+html1 +=    "<li>";
+html1 +=    "<a href=http://news.keuriggreenmountain.com>";
+html1 +=    "<img src=images/keurig.png alt=Keurig Green Mountain>";
+html1 +=    "<p>Keurig</p>";
+html1 +=    "</a>";
+html1 +=    "</li>";
+
+html1 +=    "<li>";
+html1 +=    "<a href=https://ppg3-e.newshq.businesswire.com>";
+html1 +=    "<img src=images/ppg.png alt=PPG>";
+html1 +=    "<p>PPG</p>";
+html1 +=    "</a>";
+html1 +=    "</li>";
+
+html1 +=    "<li>";
+html1 +=    "<a href=http://ir.gwrr.com>";
+html1 +=    "<img src=images/gandw.png alt=PPG>";
+html1 +=    "<p>Genesee & Wyoming</p>";
+html1 +=    "</a>";
+html1 +=    "</li>";
+
+
+html1 +=    "<li>";
+html1 +=    "<a href=https://newsroom.standard.com>";
+html1 +=    "<img src=images/standard.png alt=The Standard>";
+html1 +=    "<p>The Standard</p>";
+html1 +=    "</a>";
+html1 +=    "</li>";
+
+
+html1 +=    "<li>";
+html1 +=    "<a href=https://codepen.io/bigreed_76/pen/OXAjqb>";
+html1 +=    "<img src=images/film.png alt=Movie Quotes>";
+html1 +=    "<p>Random Movie Quotes</p>";
+html1 +=    "</a>";
+html1 +=    "</li>";
 
 html1 +=    "<li>";
 html1 +=    "<a href=http://www.girlsingerjoyce.com>";
 html1 +=    "<img src=images/joyce.png alt=GirlsingerJoyce.com>";
-html1 +=    "<p>Wordpress site</p>";
+html1 +=    "<p>Girlsinger Joyce</p>";
 html1 +=    "</a>";
 html1 +=    "</li>";
 
 html1 +=    "<li>";
 html1 +=    "<a href=../coffee/index.html>";
 html1 +=    "<img src=images/coffee.png alt=BestCoffee>";
-html1 +=    "<p>Make the best coffee</p>";
+html1 +=    "<p>The best coffee</p>";
 html1 +=    "</a>";
 html1 +=    "</li>";
-
 
 html1 +=    "<li>";
 html1 +=    "<a href=../../craps/new_craps.html>";
 html1 +=    "<img src=images/craps.png alt=CrapsGame>";
-html1 +=    "<p>Play some craps</p>";
-html1 +=    "</a>";
-html1 +=    "</li>";
-
-html1 +=    "<li>";
-html1 +=    "<a href=https://codepen.io/bigreed_76/pen/OXAjqb>";
-html1 +=    "<img src=images/film.png alt=CrapsGame>";
-html1 +=    "<p>Random Movie Quotes</p>";
+html1 +=    "<p>Craps</p>";
 html1 +=    "</a>";
 html1 +=    "</li>";
 
