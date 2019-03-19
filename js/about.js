fb945d882e479af3145a8d7a578bb4e4460728ff
rmcc0724 <rmcc0724@gmail.com>
2019-03-18 19:05:50 -0700

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,5 +1,5 @@
 // JavaScript File
 var html2="<section id=about><img class=profile-pic src=images/profilePic.jpg>";
 html2 +="<h3>About</h3>";
-html2 +="<p>Hello I'm Reid and this is my portfolio of all my projects."; 
+html2 +="<p>Hello I'm Reid and this is my portfolio of all my projects. "; 
 html2 +="Besides making Web Sites I enjoy hiking, wine tasting, being out with friends, watching movies and playing video games.</p></section>";
